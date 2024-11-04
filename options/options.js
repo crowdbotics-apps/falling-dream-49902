27027e816c78eeac7298899b3ad6f641717333a7
Crowdbotics <team@crowdbotics.com>
2024-11-04 20:21:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-dream-49902",
+  url: "https://falling-dream-49902.botics.co",
+  api: "https://falling-dream-49902.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
